503bc78d00d5f71b76666af3594ff27d565b4f90
KhiemND <khiemnd@newwave.com.vn>
2022-06-28 16:55:19 +0700

    update

--- hydra.go
+++ hydra.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
@@ -17,29 +18,30 @@ func main() {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt, syscall.SIGTERM)
 	go func() {
+		// listen terminate signal
+		// signal is received
 		<-sigchan
+		// then cancel the work
 		cancelFunc()
 	}()
 
-	go UnprocessedReporter(ctx,
-		time.Duration(4*time.Second))
+	// go UnprocessedReporter(ctx,
+	// 	time.Duration(5*time.Second))
+
 processLoop:
 	for {
 		fmt.Println(1234)
 		log.Debug("Processing data")
-		time.Sleep(time.Second * 3)
-		break processLoop
 		// err := mutator.ProcessData(ctx)
 		// if err != nil {
 		// 	panic(err)
 		// }
-		// select {
-		// case <-ctx.Done():
-		// 	externals.GetFleetlogGeofencer().Stop()
-		// 	break processLoop
-		// default:
-		// 	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(1000)))
-		// }
+		select {
+		case <-ctx.Done():
+			break processLoop
+		default:
+			time.Sleep(time.Millisecond * time.Duration(rand.Int63n(1000)))
+		}
 	}
 }
 
@@ -50,13 +52,12 @@ func UnprocessedReporter(ctx context.Context, interval time.Duration) {
 	for {
 		select {
 		case <-ticker.C:
+			fmt.Println(9999)
 			// count, err := dbBackend.StorageService().GetUnprocessedRecordCount(ctx)
 			// if err != nil {
 			// 	log.WithError(err).Error("Couldn't get unprocessed data count")
 			// 	continue
 			// }
-			// metrics.GetOrRegisterGauge(
-			// 	"hydra.unprocessed_records.total", util.CommonRegistry).Update(count)
 		case <-ctx.Done():
 			log.Debug("Stopping reporting unprocessed counts.")
 			ticker.Stop()
